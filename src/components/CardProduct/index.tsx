d63361068fd46d46284625ce853be529693b2b97
Bruno Henrique Laviso Adorne <brunolaviso@gmail.com>
2024-02-09 11:46:44 -0300

    feat: altera estilo do card de produto

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -13,8 +13,8 @@ export function CardProduct({ product }: CardProductProps) {
   const router = useRouter()
 
   return (
-    <div className="w-full md:max-w-[360px] bg-gray-100" onClick={() => router.push(`/vehicles/${product.id}`)}>
-      <img src={!!product.vehicleImage?.length ? product.vehicleImage[0]?.url_image : "/imgs/fallback-img.png"} alt="" />
+    <div className="w-full md:max-w-[360px] bg-zinc-100 hover:bg-zinc-200 cursor-pointer transition" onClick={() => router.push(`/vehicles/${product.id}`)}>
+      <img className="w-full" src={product.vehicleImage?.length ? product.vehicleImage[0]?.url_image : "/imgs/fallback-img.png"} alt="" />
       <div className="p-8">
         <h2 className="text-2xl font-semibold mb-1">{`${product.brand?.name} ${product.model?.name}`}</h2>
         <span className="text-lg">{formatCurrency(product.price)}</span>
