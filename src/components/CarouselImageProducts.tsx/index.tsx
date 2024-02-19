04c3d0e3615584e31ba7313e2d12728de6ca4e77
Bruno Henrique Laviso Adorne <brunolaviso@gmail.com>
2024-02-19 15:10:56 -0300

    feat: add btns do carousel

--- src/components/CarouselImageProducts.tsx/index.tsx
+++ src/components/CarouselImageProducts.tsx/index.tsx
@@ -1,17 +1,60 @@
 import { Vehicle } from "@/interfaces/Vehicle";
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
+import {
+  DotButton,
+  PrevButton,
+  NextButton,
+} from "./EmblaCarouselArrowsDotsButtons";
+import { EmblaCarouselType } from "embla-carousel";
 
 interface CarouselImageProductsProps {
   currentVehicle?: Vehicle;
 }
 
 import style from "./styles.module.css";
+import { useCallback, useEffect, useState } from "react";
 
 export function CarouselImageProducts({
   currentVehicle,
 }: CarouselImageProductsProps) {
-  const [emblaRef] = useEmblaCarousel({ loop: true, align: "start" }, []);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true, align: "start" },
+    []
+  );
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi]
+  );
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  );
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onInit);
+    emblaApi.on("reInit", onSelect);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onInit, onSelect]);
 
   return (
     <div className={style.embla} ref={emblaRef}>
@@ -25,6 +68,10 @@ export function CarouselImageProducts({
             />
           </div>
         ))}
+      </div>
+      <div className="embla__buttons">
+        <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+        <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
       </div>
     </div>
   );
